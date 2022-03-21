46377016317c631a56e571c431e64a05ea10d674
tek967 <79492706+tek967@users.noreply.github.com>
2022-03-21 16:33:12 +0800

    start work

--- game.py
+++ game.py
@@ -4,6 +4,11 @@ from data_io import settingsData
 class maingame:
     def __init__(self):
         db = settingsData()
-        self.winblow=open(".linerwindowsetting")
+        self.winblow = open(".linerwindowsetting")
         self.Win = pygame.display.set_mode((db.settingArray[0][1],db.settingArray[0][2]))
-        self.healthbar=10
+        self.healthbar = 100
+        self.mobDamage = 10
+        self.slipperdamage=5
+        self.mobhealth=5
+        self.littlebrother=annoying
+        
