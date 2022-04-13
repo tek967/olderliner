3dd75d9b22c4a5b44a2e4f2af05a9abf9db72a3e
ezintegg2398 <easontekreviews2398@gmail.com>
2022-04-13 10:02:48 +0800

    start a react application because why not

---
+++ reactjs/linerweb/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='head'>
+          <p className='bigTitle'>Welcome to the Liner web application!</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
